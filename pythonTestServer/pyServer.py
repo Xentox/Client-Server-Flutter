9d16d00a7fa817404bd2e9579edc937441fe2dad
Marcel <mbraun2@uni-muenster.de>
2025-10-11 01:35:04 +0200

    updated server + comments

--- pythonTestServer/pyServer.py
+++ pythonTestServer/pyServer.py
@@ -6,48 +6,67 @@ from typing import Set
 
 active: Set[WebSocket] = set()
 
-#Serverstart mit:
+# start server with:
 # python -m uvicorn pyServer:app --reload --host 0.0.0.0 --port 8000
 
-#Server schließt mit Strg+C
+# close server with Ctrl+C
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Startphase
     async def ticker():
         while True:
             await asyncio.sleep(5)
-            for peer in list(active):
-                try:
-                    await peer.send_text("SERVER-TICK: 5s vergangen")
-                except Exception:
-                    active.discard(peer)
     task = asyncio.create_task(ticker())
     yield
-    # Shutdownphase
     task.cancel()
 
 app = FastAPI(lifespan=lifespan)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],        # für Tests offen
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-@app.get("/health")
-def health():
-    return {"ok":True}
+
 
 @app.websocket("/ws")
 async def ws_endpoint(ws: WebSocket):
-    print("WS handshake incoming")  # <-- MUSS beim Verbindungsversuch erscheinen
+    #New Connection
+    print("New Connection")  
     await ws.accept()
-    await ws.send_text("SERVER: Verbunden")
+    active.add(ws)
+    await ws.send_text("connected")
+    for peer in list(active):
+        if peer != ws:
+            try:
+                await peer.send_text("New user connected")
+            except Exception:
+                active.discard(peer)
+
+    #Process Messages
     try:
         while True:
             msg = await ws.receive_text()
-            await ws.send_text(f"ECHO: {msg}")
+            
+            # Ping the server
+            if msg == "ping":
+                await ws.send_text("pong")
+
+            # sends received messages to all other clients
+            else:
+                for peer in list(active):
+                    if peer != ws:
+                        try:
+                            await peer.send_text(msg)
+                        except Exception:
+                            active.discard(peer)
+                    # Debug Option
+                    #else:
+                        # await peer.send_text(f"Message sent")
+
+    #Process Disconnection
     except WebSocketDisconnect:
-        print("WS disconnected")
+        print("disconnected")
+        active.discard(ws)
+        for peer in list(active):
+            try:
+                await peer.send_text("User disconnected")
+            except Exception:
+                active.discard(peer)
+                await peer.send_text("User disconnected")
